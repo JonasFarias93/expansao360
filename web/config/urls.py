140e89f1d3028fe404fd8442b0d4d4d3ea0e1012
JonasFarias93 <jhonas.jr@hotmail.com>
2026-01-22 06:38:05 -0300

    chore(web): configurar media local e ignorar uploads

--- web/config/urls.py
+++ web/config/urls.py
@@ -1,6 +1,8 @@
 # web/config/urls.py
 
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,3 +10,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("execucao/", include("execucao.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
