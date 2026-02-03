74bdb670368220352515e061b268228d5cfffc4b
JonasFarias93 <jhonas.jr@hotmail.com>
2026-02-03 18:45:42 -0300

    feat(admin): melhora cadastro de tipos e kit item

--- web/cadastro/admin.py
+++ web/cadastro/admin.py
@@ -8,26 +8,40 @@ from .models import (
     Loja,
     Projeto,
     Subprojeto,
+    TipoEquipamento,
 )
+
+
+class TipoEquipamentoInline(admin.TabularInline):
+    model = TipoEquipamento
+    extra = 1
 
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
     search_fields = ("nome",)
+    inlines = [TipoEquipamentoInline]
+
+
+@admin.register(TipoEquipamento)
+class TipoEquipamentoAdmin(admin.ModelAdmin):
+    list_display = ("codigo", "nome", "categoria", "ativo")
+    list_filter = ("categoria", "ativo")
+    search_fields = ("codigo", "nome", "categoria__nome")
 
 
 @admin.register(Equipamento)
 class EquipamentoAdmin(admin.ModelAdmin):
     list_display = ("codigo", "nome", "categoria", "tem_ativo", "configuravel")
     list_filter = ("categoria", "tem_ativo", "configuravel")
-
-    search_fields = ("codigo", "nome")
+    search_fields = ("codigo", "nome", "categoria__nome")
 
 
 @admin.register(Loja)
 class LojaAdmin(admin.ModelAdmin):
-    list_display = ("codigo", "nome")
-    search_fields = ("codigo", "nome")
+    list_display = ("codigo", "nome", "cidade", "uf", "logomarca")
+    list_filter = ("uf", "logomarca")
+    search_fields = ("codigo", "nome", "cidade")
 
 
 class SubprojetoInline(admin.TabularInline):
@@ -45,12 +59,16 @@ class ProjetoAdmin(admin.ModelAdmin):
 class ItemKitInline(admin.TabularInline):
     model = ItemKit
     extra = 0
-    fields = (
-        "equipamento",
-        "tipo",
-        "quantidade",
-        "requer_configuracao",
-    )
+    fields = ("equipamento", "tipo", "quantidade", "requer_configuracao")
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "tipo":
+            kwargs["queryset"] = TipoEquipamento.objects.filter(ativo=True).order_by(
+                "categoria__nome", "nome"
+            )
+        if db_field.name == "equipamento":
+            kwargs["queryset"] = Equipamento.objects.order_by("categoria__nome", "nome")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 @admin.register(Kit)
