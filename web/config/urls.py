c9e75a7a3da1c0aab4b5f9de29032710db6d4988
JonasFarias93 <jhonas.jr@hotmail.com>
2026-02-08 20:29:25 -0300

    fix(iam): use PermissionDenied and register handler403

--- web/config/urls.py
+++ web/config/urls.py
@@ -1,11 +1,11 @@
 # web/config/urls.py
 
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
+from iam.views import acesso_negado_403
 
 
 def root_redirect(request):
@@ -19,7 +19,7 @@ urlpatterns = [
     path("execucao/", include(("execucao.urls", "execucao"), namespace="execucao")),
     # Registry (cadastro)
     path("cadastro/", include(("cadastro.urls", "cadastro"), namespace="cadastro")),
-    # IAM (se você expõe alguma tela/admin extra; senão pode remover)
+    # IAM
     path("iam/", include(("iam.urls", "iam"), namespace="iam")),
     path("redes/", include(("redes.urls", "redes"), namespace="redes")),
 ]
@@ -27,3 +27,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler403 = acesso_negado_403
