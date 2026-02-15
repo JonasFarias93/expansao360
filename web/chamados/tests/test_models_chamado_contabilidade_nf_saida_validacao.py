178e8a36a2bd3fb10e997583214a316a381f5805
JonasFarias93 <jhonas.jr@hotmail.com>
2026-02-15 16:48:42 -0300

    test(chamados): padronizar nomes do contrato de validacao fiscal no Chamado

--- web/chamados/tests/test_models_chamado_contabilidade_nf_saida_validacao.py
+++ web/chamados/tests/test_models_chamado_contabilidade_nf_saida_validacao.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
+
 from django.core.exceptions import ValidationError
 from execucao.models import NF_SAIDA_ONLY_DIGITS_ERROR, Chamado
 from execucao.tests._base import ChamadoBaseTestCase
 
 
-class ChamadoContabilidadeNFSaidaValidacaoTests(ChamadoBaseTestCase):
+class TestChamadoFiscalFieldsCleanValidation(ChamadoBaseTestCase):
     def setUp(self) -> None:
         super().setUp()
         self.chamado = Chamado.objects.create(
@@ -15,18 +17,25 @@ class ChamadoContabilidadeNFSaidaValidacaoTests(ChamadoBaseTestCase):
             tipo=Chamado.Tipo.ENVIO,
         )
 
-    def test_contabilidade_numero_trim(self) -> None:
+    def test_quando_full_clean_entao_contabilidade_numero_e_trimado(self) -> None:
         self.chamado.contabilidade_numero = "  PED-001  "
+
         self.chamado.full_clean()
+
         self.assertEqual(self.chamado.contabilidade_numero, "PED-001")
 
-    def test_nf_saida_remove_espacos(self) -> None:
+    def test_quando_full_clean_entao_nf_saida_remove_espacos(self) -> None:
         self.chamado.nf_saida_numero = " 123 45 "
+
         self.chamado.full_clean()
+
         self.assertEqual(self.chamado.nf_saida_numero, "12345")
 
-    def test_nf_saida_rejeita_nao_digitos(self) -> None:
+    def test_quando_full_clean_e_nf_saida_tem_nao_digitos_entao_lanca_validation_error(
+        self,
+    ) -> None:
         self.chamado.nf_saida_numero = "NF-123"
+
         with self.assertRaises(ValidationError) as ctx:
             self.chamado.full_clean()
 
