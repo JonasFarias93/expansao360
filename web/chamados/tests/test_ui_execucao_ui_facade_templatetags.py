84bccdfcd15bc3002da5455fe01d2b436d099f73
JonasFarias93 <jhonas.jr@hotmail.com>
2026-02-15 17:39:42 -0300

    test(execucao): mover contrato do facade execucao_ui (projeto_color_bar)

--- web/chamados/tests/test_ui_execucao_ui_facade_templatetags.py
+++
@@ -1,8 +0,0 @@
-from types import SimpleNamespace
-
-from web.execucao.templatetags.execucao_ui import projeto_color_bar
-
-
-def test_execucao_ui_reexports_projeto_color_bar():
-    projeto = SimpleNamespace(cor_slug="blue")
-    assert projeto_color_bar(projeto) == "bg-blue-500"
