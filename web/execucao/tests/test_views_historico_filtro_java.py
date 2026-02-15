b0275e16174ffdbb1a1ba8fdcdbac4f2cc9c94bf
JonasFarias93 <jhonas.jr@hotmail.com>
2026-02-15 17:16:21 -0300

    test(execucao): mover e padronizar contrato do historico filtro java

--- web/execucao/tests/test_views_historico_filtro_java.py
+++ web/execucao/tests/test_views_historico_filtro_java.py
@@ -1,14 +1,16 @@
+from __future__ import annotations
+
 from django.urls import reverse
 from execucao.models import Chamado
 from execucao.tests._base import WebAuthBaseTestCase, grant_cap
 
 
-class HistoricoFiltroJavaTests(WebAuthBaseTestCase):
+class TestHistoricoFiltroJavaView(WebAuthBaseTestCase):
     def setUp(self) -> None:
         super().setUp()
         grant_cap(self.user, "execucao.chamado.visualizar")
 
-    def test_historico_busca_por_java_codigo(self) -> None:
+    def test_quando_filtra_por_java_entao_retorna_apenas_chamados_da_loja(self) -> None:
         self.loja.codigo = "02"
         self.loja.nome = "Teste Loja"
         self.loja.save()
@@ -20,19 +22,18 @@ class HistoricoFiltroJavaTests(WebAuthBaseTestCase):
             projeto=self.projeto,
             subprojeto=self.sub,
             kit=self.kit,
-            status="ABERTO",
+            status=Chamado.Status.ABERTO,
         )
         c2 = Chamado.objects.create(
             loja=loja_12,
             projeto=self.projeto,
             subprojeto=self.sub,
             kit=self.kit,
-            status="ABERTO",
+            status=Chamado.Status.ABERTO,
         )
 
         url = reverse("execucao:historico")
         resp = self.client.get(url, {"java": "02"})
-
         self.assertEqual(resp.status_code, 200)
 
         html = resp.content.decode("utf-8")
