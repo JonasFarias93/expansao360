a5fdba7db3ed091cc4e8a92d4aeec195e29ccda7
JonasFarias93 <jhonas.jr@hotmail.com>
2026-02-15 15:19:58 -0300

    test(execucao): refinar contrato de redirects do endpoint chamado_abrir

--- web/execucao/tests/test_views_chamado_abrir_redirecionamentos.py
+++ web/execucao/tests/test_views_chamado_abrir_redirecionamentos.py
@@ -7,7 +7,7 @@ from execucao.models import Chamado, ExecutionSession
 from execucao.tests._base import WebAuthBaseTestCase
 
 
-class ChamadoAbrirIniciaSessaoTests(WebAuthBaseTestCase):
+class TestChamadoAbrirRedirecionamentos(WebAuthBaseTestCase):
     def setUp(self) -> None:
         super().setUp()
         self.chamado = Chamado.objects.create(
@@ -17,10 +17,15 @@ class ChamadoAbrirIniciaSessaoTests(WebAuthBaseTestCase):
             kit=self.kit,
             status=Chamado.Status.ABERTO,
         )
+        self.url_abrir = reverse(
+            "execucao:chamado_abrir",
+            kwargs={"chamado_id": self.chamado.id},
+        )
 
-    def test_abrir_cria_sessao_e_redireciona_para_setup_quando_aberto(self) -> None:
-        url = reverse("execucao:chamado_abrir", kwargs={"chamado_id": self.chamado.id})
-        resp = self.client.post(url)
+    def test_quando_abre_chamado_aberto_entao_cria_sessao_e_redireciona_setup(
+        self,
+    ) -> None:
+        resp = self.client.post(self.url_abrir)
 
         self.assertEqual(ExecutionSession.objects.count(), 1)
         sess = ExecutionSession.objects.first()
@@ -34,23 +39,25 @@ class ChamadoAbrirIniciaSessaoTests(WebAuthBaseTestCase):
             reverse("execucao:chamado_setup", kwargs={"chamado_id": self.chamado.id}),
         )
 
-    def test_abrir_reentra_quando_mesmo_usuario(self) -> None:
+    def test_quando_reentra_mesmo_usuario_entao_redireciona_e_nao_cria_nova_sessao(
+        self,
+    ) -> None:
         ExecutionSession.objects.create(chamado=self.chamado, usuario=self.user)
 
-        url = reverse("execucao:chamado_abrir", kwargs={"chamado_id": self.chamado.id})
-        resp = self.client.post(url)
+        resp = self.client.post(self.url_abrir)
 
         self.assertEqual(ExecutionSession.objects.count(), 1)
         self.assertEqual(resp.status_code, 302)
 
-    def test_abrir_bloqueia_quando_outro_usuario(self) -> None:
+    def test_quando_sessao_ativa_de_outro_usuario_entao_redireciona_detalhe_e_nao_cria_nova(
+        self,
+    ) -> None:
         User = get_user_model()
         other = User.objects.create_user(username="u2", password="x")
 
         ExecutionSession.objects.create(chamado=self.chamado, usuario=other)
 
-        url = reverse("execucao:chamado_abrir", kwargs={"chamado_id": self.chamado.id})
-        resp = self.client.post(url)
+        resp = self.client.post(self.url_abrir)
 
         self.assertEqual(ExecutionSession.objects.count(), 1)
         self.assertEqual(resp.status_code, 302)
